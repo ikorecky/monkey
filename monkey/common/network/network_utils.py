c03c66bf7412f9c705d6454290bc67060d94e3ab
Ilija Lazoroski <ilija.la@live.com>
2022-08-19 13:56:53 +0200

    Common: address_to_ip_port to accept http://<address>:<port>

--- monkey/common/network/network_utils.py
+++ monkey/common/network/network_utils.py
@@ -2,6 +2,9 @@ from typing import Optional, Tuple
 
 
 def address_to_ip_port(address: str) -> Tuple[str, Optional[str]]:
+    if "http://" in address:
+        address = address.replace("http://", "")
+
     if ":" in address:
         ip, port = address.split(":")
         return ip, port or None
--- monkey/tests/unit_tests/common/network/test_network_utils.py
+++ monkey/tests/unit_tests/common/network/test_network_utils.py
@@ -7,6 +7,12 @@ def test_address_to_ip_port():
     assert port == "5000"
 
 
+def test_proxy_address_to_ip_port():
+    ip, port = address_to_ip_port("http://192.168.65.1:5000")
+    assert ip == "192.168.65.1"
+    assert port == "5000"
+
+
 def test_address_to_ip_port_no_port():
     ip, port = address_to_ip_port("192.168.65.1")
     assert port is None
